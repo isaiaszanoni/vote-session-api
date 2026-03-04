c305f3fa7be7e5fe52dd2ca8884c585d09850c05
isaiaszanoni <isaiasz.mus@gmail.com>
2026-03-04 17:57:32 -0300

    fix: swagger cors problem

--- src/main/java/com/siccase/vote_session_api/controller/TopicControllerV1.java
+++ src/main/java/com/siccase/vote_session_api/controller/TopicControllerV1.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/topics")
 @AllArgsConstructor
